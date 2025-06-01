4b3b59f31c5945edbe71fb858f28b495e7db51c3
Fergus Lai <ferguslai724@gmail.com>
2025-06-01 22:37:00 +0100

    Fix Error Line Component Naming

--- src/component/ErrorLine.tsx
+++ src/component/ErrorLine.tsx
@@ -4,7 +4,7 @@ interface Props {
   commandError: CommandError;
 }
 
-export default function CommandLine({ commandError }: Props) {
+export default function ErrorLine({ commandError }: Props) {
   return (
     <div className="flex flex-row px-4 font-mono w-full text-white">
       bash: {commandError.command}: {commandError.error}
