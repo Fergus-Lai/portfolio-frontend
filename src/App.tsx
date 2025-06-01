b0c97d269b9cb1136c4cc49da0c1d56a15d26d94
Fergus Lai <ferguslai724@gmail.com>
2025-06-01 15:28:37 +0100

    Command Line New Line On Enter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import CommandLine from "./component/commandline";
 
 function App() {
+  const [commands, setCommands] = useState([""]);
+  const commandSave = (newCommand: string) => {
+    setCommands((prevCommands) => {
+      if (prevCommands.length === 0) return prevCommands;
+      const newCommands = [...prevCommands];
+      newCommands[newCommands.length - 1] = newCommand;
+      newCommands.push("");
+      return newCommands;
+    });
+  };
+
   return (
-    <main className="flex min-h-screen min-w-screen h-full w-full bg-terminal-purple">
-      <CommandLine />
+    <main className="flex min-h-screen min-w-screen h-full w-full bg-terminal-purple flex-col">
+      {commands.map((command, i) => (
+        <CommandLine command={command} commandSave={commandSave} key={i} />
+      ))}
     </main>
   );
 }
--- src/component/CommandLine.tsx
+++ src/component/CommandLine.tsx
@@ -1,11 +1,34 @@
-export default function CommandLine() {
+import { useState } from "react";
+
+interface Props {
+  command: string;
+  commandSave: (newCommand: string) => void;
+}
+
+export default function CommandLine({ command, commandSave }: Props) {
+  const [disabled, setDisabled] = useState(false);
+  const [commandState, setCommandState] = useState(command);
   return (
     <div className="flex flex-row p-4 font-mono">
       <p className="font-bold text-terminal-green">visitor@fergus-portfolio</p>
       <p className="text-white">:</p>
       <p className="text-terminal-blue">~</p>
       <p className="text-white pr-2">$</p>
-      <input className="focus:outline-0 text-white h-6" />
+      <input
+        className="focus:outline-0 text-white h-6"
+        type="text"
+        disabled={disabled}
+        value={commandState}
+        onChange={(e) => {
+          setCommandState(e.target.value);
+        }}
+        onKeyDown={(e) => {
+          if (e.key == "Enter") {
+            setDisabled(true);
+            commandSave(command);
+          }
+        }}
+      />
     </div>
   );
 }
