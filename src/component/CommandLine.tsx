0e28c3d24d27059bd3c3c47e30401b8c9ad9d586
Fergus Lai <ferguslai724@gmail.com>
2025-06-01 21:33:51 +0100

    Command Line Width Fill Whole Page

--- src/component/CommandLine.tsx
+++ src/component/CommandLine.tsx
@@ -10,13 +10,13 @@ export default function CommandLine({ command, commandSave }: Props) {
   const [disabled, setDisabled] = useState(false);
   const [commandState, setCommandState] = useState(command.command);
   return (
-    <div className="flex flex-row px-4 font-mono">
+    <div className="flex flex-row px-4 font-mono w-full">
       <p className="font-bold text-terminal-green">visitor@fergus-portfolio</p>
       <p className="text-white">:</p>
       <p className="text-terminal-blue">{command.path}</p>
       <p className="text-white pr-2">$</p>
       <input
-        className="focus:outline-0 text-white h-6"
+        className="focus:outline-0 text-white h-6 flex-auto"
         type="text"
         disabled={disabled}
         value={commandState}
