b5986967a51a5f8407cb319f87382c90c7c96f66
Fergus Lai <ferguslai724@gmail.com>
2025-06-02 18:35:22 +0100

    Auto highlight link

--- src/component/CatLine.tsx
+++ src/component/CatLine.tsx
@@ -1,11 +1,22 @@
 interface Props {
   content: string;
 }
+import DOMPurify from "dompurify";
+
+const linkify = (text: string) =>
+  text.replace(
+    /(https?:\/\/[^\s]+)/g,
+    '<a href="$1" target="_blank" class="underline">$1</a>'
+  );
 
 export default function CatLine({ content }: Props) {
+  const safeHtml = DOMPurify.sanitize(linkify(content));
+
   return (
     <div className="flex flex-row px-4 font-mono w-full text-white">
-      <pre>{content}</pre>
+      <pre style={{ whiteSpace: "pre-wrap" }}>
+        <code dangerouslySetInnerHTML={{ __html: safeHtml }} />
+      </pre>
     </div>
   );
 }
