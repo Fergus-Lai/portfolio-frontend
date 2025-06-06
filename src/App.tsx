e3fb70a9973ee6f0b256461997892062b49a1d40
Fergus Lai <ferguslai724@gmail.com>
2025-06-06 03:46:10 +0100

    Use mobile whole viewport

--- src/App.tsx
+++ src/App.tsx
@@ -26,64 +26,66 @@ export default function App() {
   const [experienceTab, setExperienceTab] = useState(true);
 
   return (
-    <main className="flex flex-col h-screen w-screen">
-      <div className="flex flex-row bg-zinc-800 text-white divide-x divide-zinc-500">
-        <NavigatorTab
-          tabIndex={tabIndex}
-          setTabIndex={setTabIndex}
-          selfIndex={0}
-          text="Terminal"
-        />
-        <NavigatorTab
-          tabIndex={tabIndex}
-          setTabIndex={setTabIndex}
-          selfIndex={1}
-          text="Internet Browser"
-        />
-        <NavigatorTab
-          tabIndex={tabIndex}
-          setTabIndex={setTabIndex}
-          selfIndex={2}
-          text="Contact Me"
-        />
-        <NavigatorTab
-          tabIndex={tabIndex}
-          setTabIndex={setTabIndex}
-          selfIndex={3}
-          text="Admin"
-        />
+    <main className="absolute inset-0">
+      <div className="flex flex-col h-full w-full">
+        <div className="flex flex-row bg-zinc-800 text-white divide-x divide-zinc-500">
+          <NavigatorTab
+            tabIndex={tabIndex}
+            setTabIndex={setTabIndex}
+            selfIndex={0}
+            text="Terminal"
+          />
+          <NavigatorTab
+            tabIndex={tabIndex}
+            setTabIndex={setTabIndex}
+            selfIndex={1}
+            text="Internet Browser"
+          />
+          <NavigatorTab
+            tabIndex={tabIndex}
+            setTabIndex={setTabIndex}
+            selfIndex={2}
+            text="Contact Me"
+          />
+          <NavigatorTab
+            tabIndex={tabIndex}
+            setTabIndex={setTabIndex}
+            selfIndex={3}
+            text="Admin"
+          />
+        </div>
+        {tabIndex == 0 ? (
+          <Terminal
+            terminalRecords={terminalRecords}
+            setTerminalRecords={setTerminalRecords}
+            commandHistory={commandHistory}
+            setCommandHistory={setCommandHistory}
+            showWelcome={showWelcome}
+            setShowWelcome={setShowWelcome}
+            path={path}
+            setPath={setPath}
+          />
+        ) : tabIndex == 1 ? (
+          <BrowserTab
+            experienceTab={experienceTab}
+            setExperienceTab={setExperienceTab}
+            toContact={() => setTabIndex(2)}
+          />
+        ) : tabIndex == 2 ? (
+          <ContactMe
+            title={title}
+            setTitle={setTitle}
+            email={email}
+            setEmail={setEmail}
+            inputName={inputName}
+            setInputName={setInputName}
+            message={message}
+            setMessage={setMessage}
+          />
+        ) : (
+          <Admin />
+        )}
       </div>
-      {tabIndex == 0 ? (
-        <Terminal
-          terminalRecords={terminalRecords}
-          setTerminalRecords={setTerminalRecords}
-          commandHistory={commandHistory}
-          setCommandHistory={setCommandHistory}
-          showWelcome={showWelcome}
-          setShowWelcome={setShowWelcome}
-          path={path}
-          setPath={setPath}
-        />
-      ) : tabIndex == 1 ? (
-        <BrowserTab
-          experienceTab={experienceTab}
-          setExperienceTab={setExperienceTab}
-          toContact={() => setTabIndex(2)}
-        />
-      ) : tabIndex == 2 ? (
-        <ContactMe
-          title={title}
-          setTitle={setTitle}
-          email={email}
-          setEmail={setEmail}
-          inputName={inputName}
-          setInputName={setInputName}
-          message={message}
-          setMessage={setMessage}
-        />
-      ) : (
-        <Admin />
-      )}
     </main>
   );
 }
